2a0abcee555be335d5207f8839935dedb0a9855b
jisidaisy <jisidaisy@yahoo.com>
2023-02-15 16:10:57 -0600

    Title: Added the first function
    Description: Added the function to contain the first test case for add

---
+++ test_statistics.py
@@ -0,0 +1,26 @@
+import Statistics
+
+def test_add():
+    '''
+    Purpose:
+        Test the add() method of the Statistics class.
+    Pre-conditions:
+        (none)
+    Post-conditions:
+        (none)
+    Return:
+        :return: True if the test passes, False otherwise.
+    '''
+    s = Statistics.Statistics()
+    s.add(1)
+    s.add(2)
+    s.add(3)
+    s.add(4)
+    s.add(5)
+    if s.count() != 5:
+        print("Error in add(): incorrect count")
+        return False
+    if s.mean() != 3:
+        print("Error in add(): incorrect mean")
+        return False
+    return True
